4e6ac2c9adb80b4e88373adccd724620d5b6d734
Nadezhda Svezhintseva <svezhintzeva@ro46.fss.ru>
2016-12-02 17:05:11 +0300

    Cli version was updated

--- src/app/contest/contest.component.spec.ts
+++ src/app/contest/contest.component.spec.ts
@@ -1,11 +1,33 @@
-/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-import { TestBed, async } from '@angular/core/testing';
-import { ContestComponent } from './contest.component';
+import { ContestComponent} from './contest.component';
 
-describe('Component: Contest', () => {
-  it('should create an instance', () => {
-    let component = new ContestComponent();
-    expect(component).toBeTruthy();
+let comp:    ContestComponent;
+let fixture: ComponentFixture<ContestComponent>;
+let de:      DebugElement;
+let el:      HTMLElement;
+
+describe('ContestComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContestComponent ], // declare the test component
+    });
+
+    fixture = TestBed.createComponent(ContestComponent);
+
+    comp = fixture.componentInstance; // Component test instance
+
+    // query for the title .contest-header__name by CSS element selector
+    de = fixture.debugElement.query(By.css('.contest-header__name'));
+    el = de.nativeElement;
+
   });
+
+  it('should display original title', () => {
+  fixture.detectChanges();
+  expect(el.textContent).toContain(comp.contest.name);
+	});
+
 });
